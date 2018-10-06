aa07bd65589756b3e30da85c15f8fbbfd075e184
Ray Becoskie <ray@becoskie.com>
2018-10-06 17:15:03 -0500

    Got pages Routed

---
+++ client/src/pages/dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Dashboard.css";
+
+class Search extends Component {
+  
+
+  render() {
+    return (
+      <div className="container col-10 offset-1">
+        <h1>Dashboard</h1>
+      </div>
+    );
+  }
+}
+
+export default Search;
