d520b35f95745fce3b18665027bd6165c3720b0b
Ray Becoskie <ray@becoskie.com>
2018-10-07 16:16:19 -0500

    Wired up the server to the client

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const morgan = require('morgan');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan('dev'))
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+app.post('/', (req, res, next)=> {
+	console.log('server post username: ');
+	console.log(req.body.username)
+	res.end()
+})
 
 // Send every other request to the React app
 // Define any API routes before this runs
