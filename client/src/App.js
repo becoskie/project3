45dead3b3031001cc8d20fae51d57914b631ad45
Ray Becoskie <ray@becoskie.com>
2018-10-07 15:59:41 -0500

    The signup page to the routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
 import './App.css';
 // import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
@@ -15,6 +16,7 @@ const App = () => (
       <Nav />
       <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/signup" component={SignUp} />
       <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
     </div>
---
+++ passport/localStrategy.js
@@ -0,0 +1,25 @@
+
+const LocalStrategy = require('passport-local').Strategy
+//const UserLocal = require('../models/user')
+
+const strategy = new LocalStrategy(
+	{
+		usernameField: 'username' // not necessary, DEFAULT
+	},
+	function(username, password, done) {
+		User.findOne({ username: username }, (err, user) => {
+			if (err) {
+				return done(err)
+			}
+			if (!user) {
+				return done(null, false, { message: 'Incorrect username' })
+			}
+			if (!user.checkPassword(password)) {
+				return done(null, false, { message: 'Incorrect password' })
+			}
+			return done(null, user)
+		})
+	}
+)
+
+module.exports = strategy
