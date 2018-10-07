9caa6c0b5b2e4e78fbb3b9e3d9050e66f1c079d8
Ray Becoskie <ray@becoskie.com>
2018-10-07 16:16:19 -0500

    Wired up the server to the client

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -9,7 +9,7 @@ class SignUp extends Component {
 			username: '',
 			password: '',
 			confirmPassword: '',
-			
+
 		}
 		this.handleSubmit = this.handleSubmit.bind(this)
 		this.handleChange = this.handleChange.bind(this)
@@ -21,32 +21,76 @@ class SignUp extends Component {
 	}
 	handleSubmit(event) {
 		event.preventDefault()
-        console.log('sign-up-form, username: ');
-        console.log(this.state.username);
+		console.log('sign-up-form, username: ');
+		console.log(this.state.username);
 		//request to server here
+		axios.post('/', {
+			username: this.state.username,
+			password: this.state.password
+		})
+			.then(response => {
+				console.log(response)
+				if (response.data) {
+					console.log('successful signup')
+					this.setState({
+						redirectTo: '/login'
+					})
+				} else {
+						console.log('Sign-up error');
+						
+					}
+			}).catch(error => {
+				console.log('Sign up server error: ')
+				console.log(error);
+			})
 	}
 	render() {
 
 		return (
 			<div className="SignupForm">
-				<h1>Sign Up form</h1>
-				<label htmlFor="username">Username: </label>
-				<input
-					type="text"
-					name="username"
-					value={this.state.username}
-					onChange={this.handleChange}
-				/>
-				<label htmlFor="password">Password: </label>
-				<input
-					type="password"
-					name="password"
-					value={this.state.password}
-					onChange={this.handleChange}
-				/>
-				
-				<button onClick={this.handleSubmit}>Sign up</button>
+
+				<form className="form-horizontal">
+					<div className="form-group">
+						<div className="col-1 col-ml-auto">
+							<label className="form-label" htmlFor="username">Username</label>
+						</div>
+						<div className="col-3 col-mr-auto">
+							<input className="form-input"
+								type="text"
+								id="input-example-1"
+								placeholder="Name"
+								name="username"
+								value={this.state.username}
+								onChange={this.handleChange}
+							/>
+						</div>
+					</div>
+					<div className="form-group">
+						<div className="col-1 col-ml-auto">
+							<label className="form-label" htmlFor="password">Password: </label>
+						</div>
+						<div className="col-3 col-mr-auto">
+							<input className="form-input"
+								placeholder="Password"
+								type="password"
+								name="password"
+								value={this.state.password}
+								onChange={this.handleChange}
+							/>
+						</div>
+
+					</div>
+
+					<div className="form-group ">
+						<div className="col-7"></div>
+						
+							<button className="btn btn-primary col-1 col-mr-auto" onClick={this.handleSubmit}>Sign up</button>
+						
+
+					</div>
+				</form>
 			</div>
+
 		)
 	}
 }
