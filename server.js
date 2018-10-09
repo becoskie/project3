1e12c29241362db34579861e894885b761e1f103
Ray Becoskie <ray@becoskie.com>
2018-10-08 19:39:56 -0500

    Added user models

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const morgan = require('morgan');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const user = require('./controllers/user');
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,11 +23,26 @@ app.post('/', (req, res, next)=> {
 	res.end()
 })
 
+app.use('/user', user)
+
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+const db = process.env.MONGODB_URI || "mongodb://localhost/project-3";
+mongoose.connect(db, function(error) {
+  // Log any errors connecting with mongoose
+  if (error) {
+    console.error(error);
+  }
+  // Or log a success message
+  else {
+    console.log("mongoose connection is successful");
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
