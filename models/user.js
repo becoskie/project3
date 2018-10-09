7cd58b0d05a22a22436a5f60ba04ecf74a3d9145
Ray Becoskie <ray@becoskie.com>
2018-10-08 21:26:27 -0500

    Somewhat working

--- models/user.js
+++ models/user.js
@@ -1,15 +1,39 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
-mongoose.promise = Promise
+const Schema = mongoose.Schema;
 
-// Define userSchema
 const userSchema = new Schema({
-
-	username: { type: String, unique: false, required: false },
+    username: { type: String, unique: false, required: false },
 	password: { type: String, unique: false, required: false }
+});
 
+// Define schema methods
+userSchema.methods = {
+	checkPassword: function (inputPassword) {
+		return bcrypt.compareSync(inputPassword, this.password)
+	},
+	hashPassword: plainTextPassword => {
+		return bcrypt.hashSync(plainTextPassword, 10)
+	}
+}
+
+// Define pre hooks for the save method
+userSchema.pre('save', function (next) {
+	if (!this.password) {
+		console.log('models/user.js =======NO PASSWORD PROVIDED=======')
+		next()
+	} else {
+		console.log('models/user.js hashPassword in pre save');
+		
+		this.password = this.hashPassword(this.password)
+		next()
+	}
 })
 
-const User = mongoose.model('User', userSchema)
-module.exports = User
+console.log("CREATE USER:")
+
+const User = mongoose.model("User", userSchema);
+
+
+module.exports = User;
